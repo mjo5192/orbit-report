308491ea48bb840a61b6efe33ed7f331eec46dcd
Matt Owens <56141186+mjo5192@users.noreply.github.com>
2019-11-06 18:49:07 -0600

    Add files via upload

---
+++ src/app/satellite.ts
@@ -0,0 +1,12 @@
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+
+class Satellite {
+    name: string;
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+
+    }
+}
